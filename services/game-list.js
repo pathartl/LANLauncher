551aa3761abf68780f09c012c633ca939aeffe2e
Pat Hartl <pat@pathar.tl>
2016-08-29 20:46:41 -0500

    Get installed games on startup

--- services/game-list.js
+++ services/game-list.js
@@ -130,6 +130,7 @@ class GameListService {
 		this.renderGameList();
 		this.renderGameFilter();
 		this.enableFilterInteraction();
+		this.updateInstalledGameList();
 	}
 
 	updateInstalledGameList() {
